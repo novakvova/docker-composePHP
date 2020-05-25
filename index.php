b6dfebc3d956191e11c67d32fce7db9eb3de72c7
novakvova <novakvova@gmail.com>
2020-05-25 15:11:43 +0300

    conn php

--- index.php
+++ index.php
@@ -1,62 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php include("style.php"); ?>
-    <title>Document</title>
-</head>
-<body>
-
-<?php include("navbar.php") ?>
-<?php include_once ("connection_database.php");?>
-<div class="container">
-    <div class="row">
-        <h1>Головна сторінка</h1>
-
-        <table class="table table-dark">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Email</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $sql = "SELECT u.id, u.email, u.image FROM tbl_users AS u";
-            $stmt= $dbh->prepare($sql);
-            $stmt->execute();
-            while($row=$stmt->fetch(PDO::FETCH_ASSOC))
-            {
-                ?>
-                <tr>
-                    <td scope="row">
-                        <img src="/upload/<?php echo $row['image'];?> " width="100" alt="">
-                    </td>
-                    <td> <?php echo $row['email']; ?> </td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-        </table>
-
-    </div>
-</div>
-
 <?php
-//$a=2;
-//$b="23";
-//$c=$a+$b;
-//echo "<h1>Hello php ".$c."</h1>";
-//echo "<h1>".$semen."</h1>";
-//if(isset($ivan)) {
-//    echo "<h1>" . $ivan . "</h1>";
-//}
-?>
-
-<?php include("scripts.php"); ?>
-</body>
-</html>
+$host = 'mysql';
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+ 
+$conn = mysqli_connect($host, $user, $pass);
+if (!$conn) {
+    exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+}
+ 
+echo 'Successful database connection!'.PHP_EOL;
